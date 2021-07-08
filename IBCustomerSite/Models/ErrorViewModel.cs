e261c13716c13f7ecf56ffb0e23c293bc1d51b0f
s3318814 <61717348+gerardmanderson@users.noreply.github.com>
2021-07-08 23:52:18 +1000

    * .gitignore: * Assignment2.sln: * Program.cs: * Startup.cs: * appsettings.json: * site.js: * favicon.ico: * site.css: * IBCustomerSite.csproj: * Index.cshtml: * _ViewStart.cshtml: * ErrorViewModel.cs: * Privacy.cshtml: * Error.cshtml: * _ViewImports.cshtml: * _Layout.cshtml: * appsettings.Development.json: * HomeController.cs: * LICENSE: * LICENSE.txt: * launchSettings.json: * jquery.js: * jquery.min.js: * jquery.min.map: * LICENSE.md: * bootstrap.js: * bootstrap.css: * _ValidationScriptsPartial.cshtml: * bootstrap.min.js: * bootstrap.js.map: * bootstrap.css.map: * bootstrap.min.css: * bootstrap-grid.css: * bootstrap.bundle.js: * bootstrap.min.js.map: * bootstrap-reboot.css: * bootstrap.min.css.map: * LICENSE.txt: * bootstrap.bundle.js.map: * bootstrap.bundle.min.js: * jquery.validate.js: * bootstrap-grid.css.map: * bootstrap-grid.min.css: * bootstrap-reboot.css.map: * bootstrap-reboot.min.css: * additional-methods.js: * bootstrap.bundle.min.js.map: * jquery.validate.min.js: * bootstrap-grid.min.css.map: * bootstrap-reboot.min.css.map: * additional-methods.min.js: * jquery.validate.unobtrusive.js: * jquery.validate.unobtrusive.min.js: Initial commit

---
+++ IBCustomerSite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace IBCustomerSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
