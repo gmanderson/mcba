6895328538f137849d6aa809a0784b33ed1e770b
s3318814 <61717348+gerardmanderson@users.noreply.github.com>
2021-07-30 13:07:43 +1000

    Created dbcontext

---
+++ IBCustomerSite/Data/MCBAContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using IBCustomerSite.Models;
+
+namespace IBCustomerSite.Data
+{
+    public class MCBAContext : DbContext
+    {
+        public MCBAContext(DbContextOptions<MCBAContext> options) : base(options)
+        {        }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Login> Logins { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<BillPay> BillPays { get; set; }
+        public DbSet<Payee> Payees { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Transaction>().
+                HasOne(x => x.Account).WithMany(x => x.Transactions).HasForeignKey(x => x.AccountNumber);
+        }
+    }
+}
