44dbdf2af4037b9018a6cc71ec68697d1c90693b
s3318814 <61717348+gerardmanderson@users.noreply.github.com>
2021-07-30 13:02:16 +1000

    Created billpay model

---
+++ IBCustomerSite/Models/BillPay.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace IBCustomerSite.Models
+{
+    public class BillPay
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Required]
+        public int BillPayID { get; set; }
+
+        [ForeignKey("Account")]
+        [Required]
+        public int AccountNumber { get; set; }
+        public virtual Account Account { get; set; }
+
+        [ForeignKey("Payee")]
+        [Required]
+        public int PayeeID { get; set; }
+        public virtual Payee Payee { get; set; }
+
+        [Required]
+        [Column(TypeName = "money")]
+        public decimal Amount { get; set; }
+
+        [Required]
+        public DateTime ScheduleTimeUtc { get; set; }
+
+        [Required]
+        public char Period { get; set; }
+
+    }
+}
