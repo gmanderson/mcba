caf0c430164f65e04327809376a6e3fd1bdde569
s3318814 <61717348+gerardmanderson@users.noreply.github.com>
2021-07-30 13:06:16 +1000

    Created account model

--- IBCustomerSite/Models/Account.cs
+++ IBCustomerSite/Models/Account.cs
@@ -1,10 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace IBCustomerSite.Models
 {
     public class Account
     {
-        public Account()
-        {
-        }
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Required]
+        public int AccountNumber { get; set; }
+
+        [Required]
+        public char AccountType { get; set; }
+
+        [ForeignKey("Customer")]
+        [Required]
+        public int CustomerID { get; set; }
+        public virtual Customer Customer { get; set; }
+
+        public virtual List<Transaction> Transactions { get; set; }
+
+        public virtual List<BillPay> BillPays { get; set; }
     }
 }
