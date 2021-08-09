9d523c2c81e65f1001d891b3f4219baa78adcd42
s3318814 <61717348+gerardmanderson@users.noreply.github.com>
2021-08-10 00:08:33 +1000

    Added basic withdrawal and transfer, no confirmation

--- IBCustomerSite/Controllers/HomeController.cs
+++ IBCustomerSite/Controllers/HomeController.cs
@@ -85,6 +85,114 @@ namespace IBCustomerSite.Controllers
             return RedirectToAction(nameof(Index));
         }
 
+        // GET WITHDRAWAL
+        public async Task<IActionResult> Withdrawal()
+        {
+            var customer = await _context.Customers.FindAsync(CustomerID);
+
+            return View(new DepositViewModel
+            {
+                Accounts = customer.Accounts
+            }
+                );
+        }
+
+
+        // POST - WITHDRAWAL
+        [HttpPost]
+        public async Task<IActionResult> Withdrawal(DepositViewModel viewModel)
+        {
+            viewModel.Account = await _context.Accounts.FindAsync(viewModel.AccountNumber);
+
+            //if (account.Transactions.Amount <= 0)
+            //{
+            //    ModelState.AddModelError(nameof(viewModel.Amount), "Amount must be positive.");
+            //    return View(viewModel);
+            //}
+            //if (viewModel.Amount.HasMoreThanTwoDecimalPlaces())
+            //{
+            //    ModelState.AddModelError(nameof(viewModel.Amount), "Amount cannot have more than 2 decimal places.");
+            //    return View(viewModel);
+            //}
+
+            // Note this code could be moved out of the controller, e.g., into the Model.
+
+            viewModel.Account.Transactions.Add(
+    new Transaction
+    {
+        TransactionType = 'W',
+        Amount = viewModel.Amount,
+        TransactionTimeUtc = DateTime.UtcNow,
+        Comment = viewModel.Comment
+    });
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET TRANSFER
+        public async Task<IActionResult> Transfer()
+        {
+            var customer = await _context.Customers.FindAsync(CustomerID);
+
+            return View(new TransferViewModel
+            {
+                Accounts = customer.Accounts
+            }
+                );
+        }
+
+
+        // POST - TRANSFER
+        [HttpPost]
+        public async Task<IActionResult> Transfer(TransferViewModel viewModel)
+        {
+            viewModel.SourceAccount = await _context.Accounts.FindAsync(viewModel.SourceAccountNumber);
+            viewModel.DestinationAccount = await _context.Accounts.FindAsync(viewModel.DestinationAccountNumber);
+
+            //if (account.Transactions.Amount <= 0)
+            //{
+            //    ModelState.AddModelError(nameof(viewModel.Amount), "Amount must be positive.");
+            //    return View(viewModel);
+            //}
+            //if (viewModel.Amount.HasMoreThanTwoDecimalPlaces())
+            //{
+            //    ModelState.AddModelError(nameof(viewModel.Amount), "Amount cannot have more than 2 decimal places.");
+            //    return View(viewModel);
+            //}
+
+            // Note this code could be moved out of the controller, e.g., into the Model.
+
+            viewModel.SourceAccount.Transactions.Add(
+    new Transaction
+    {
+        TransactionType = 'T',
+        Amount = viewModel.Amount,
+        TransactionTimeUtc = DateTime.UtcNow,
+        Comment = viewModel.Comment,
+        DestinationAccountNumber = viewModel.DestinationAccountNumber
+    });
+
+
+            
+
+            viewModel.DestinationAccount.Transactions.Add(
+                new Transaction
+                {
+                TransactionType = 'T',
+                Amount = viewModel.Amount,
+                TransactionTimeUtc = DateTime.UtcNow,
+                Comment = viewModel.Comment
+             });
+
+
+            await _context.SaveChangesAsync();
+
+
+            return RedirectToAction(nameof(Index));
+        }
+
 
         // GET ACCOUNT
         public async Task<IActionResult> Statement()
---
+++ IBCustomerSite/ViewModels/TransferViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using IBCustomerSite.Models;
+namespace IBCustomerSite.ViewModels
+{
+    public class TransferViewModel
+    {
+        public List<Account> Accounts { get; set; }
+        public Account SourceAccount { get; set; }
+        public Account DestinationAccount { get; set; }
+        public int SourceAccountNumber { get; set; }
+        public int DestinationAccountNumber { get; set; }
+        public decimal Amount { get; set; }
+        public string Comment { get; set; }
+
+    }
+}
