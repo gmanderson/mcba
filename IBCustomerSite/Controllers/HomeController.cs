00dacc56d7d1ae2c8855fb0d25e63b34678e1a13
s3318814 <61717348+gerardmanderson@users.noreply.github.com>
2021-08-09 18:48:47 +1000

    Added deposit section

--- IBCustomerSite/Controllers/HomeController.cs
+++ IBCustomerSite/Controllers/HomeController.cs
@@ -6,16 +6,21 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using IBCustomerSite.Models;
+using IBCustomerSite.ViewModels;
+using IBCustomerSite.Data;
 
 namespace IBCustomerSite.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly MCBAContext _context;
+        private readonly int CustomerID = 2100;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, MCBAContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -32,6 +37,57 @@ namespace IBCustomerSite.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        // GET DEPOSIT
+        public async Task<IActionResult> Deposit()
+        {
+            var customer = await _context.Customers.FindAsync(CustomerID);
+
+            return View(new DepositViewModel
+            {
+                Accounts = customer.Accounts
+            }
+                ) ;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Deposit()
+        {
+            var account = await _context.Accounts.FindAsync(4100);
+
+            if (account.Transactions.Amount <= 0)
+            {
+                ModelState.AddModelError(nameof(viewModel.Amount), "Amount must be positive.");
+                return View(viewModel);
+            }
+            if (viewModel.Amount.HasMoreThanTwoDecimalPlaces())
+            {
+                ModelState.AddModelError(nameof(viewModel.Amount), "Amount cannot have more than 2 decimal places.");
+                return View(viewModel);
+            }
+
+            // Note this code could be moved out of the controller, e.g., into the Model.
+            viewModel.Account.Balance += viewModel.Amount;
+            viewModel.Account.Transactions.Add(
+                new Transaction
+                {
+                    TransactionType = TransactionType.Deposit,
+                    Amount = viewModel.Amount,
+                    TransactionTimeUtc = DateTime.UtcNow
+                });
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET ACCOUNT
+        public async Task<IActionResult> Statement()
+        {
+            int accountNumber = 4100;
+            var account = await _context.Accounts.FindAsync(accountNumber);
+            return View(account);
         }
     }
 }
---
+++ IBCustomerSite/ViewModels/DepositViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using IBCustomerSite.Models;
+namespace IBCustomerSite.ViewModels
+{
+    public class DepositViewModel
+    {
+        public List<Account> Accounts { get; set; }
+        public decimal Amount { get; set; }
+        public string Comment { get; set; }
+
+    }
+}
