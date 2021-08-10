fb98416b683e8d2c2caa4caf5fbf053e55ba9433
s3318814 <61717348+gerardmanderson@users.noreply.github.com>
2021-08-10 23:56:24 +1000

    Set up controller and basic view

---
+++ IBCustomerSite/Controllers/BillPayController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using IBCustomerSite.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IBCustomerSite.Controllers
+{
+    public class BillPayController : Controller
+    {
+        private readonly MCBAContext _context;
+
+        public BillPayController(MCBAContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
