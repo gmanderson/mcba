4ac8e828d1b7079dc0795569d63f580b0e010923
s3318814 <61717348+gerardmanderson@users.noreply.github.com>
2021-08-10 15:32:36 +1000

    Added filter for authorisation on pages

---
+++ IBCustomerSite/Filters/AuthorizeCustomerAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using IBCustomerSite.Models;
+
+namespace IBCustomerSite.Filters
+{
+    public class AuthorizeCustomerAttribute : Attribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            var customerID = context.HttpContext.Session.GetInt32(nameof(Customer.CustomerID));
+            if (!customerID.HasValue)
+                context.Result = new RedirectToActionResult("Login", "Login", null);
+        }
+    }
+}
