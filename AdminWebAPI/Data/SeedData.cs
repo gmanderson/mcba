4f68cabf548bb11fb08de82ed78d9251c3dcb67f
s3318814 <61717348+gerardmanderson@users.noreply.github.com>
2021-08-12 19:22:49 +1000

    Added migrations

---
+++ AdminWebAPI/Data/SeedData.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Linq;
+using AdminWebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using AdminWebAPI.Data;
+
+namespace AdminWebAPI
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using var context = new MCBAContext(serviceProvider.GetRequiredService<DbContextOptions<MCBAContext>>());
+
+            // Look for customers.
+            if (context.Customers.Any())
+                return; // DB has already been seeded.
+
+            context.Customers.AddRange(
+                new Customer
+                {
+                    CustomerID = 2100,
+                    Name = "Matthew Bolger",
+                    Address = "123 Fake Street",
+                    Suburb = "Melbourne",
+                    Postcode = "3000"
+                },
+                new Customer
+                {
+                    CustomerID = 2200,
+                    Name = "Rodney Cocker",
+                    Address = "456 Real Road",
+                    Suburb = "Melbourne",
+                    Postcode = "3005"
+                },
+                new Customer
+                {
+                    CustomerID = 2300,
+                    Name = "Shekhar Kalra"
+                });
+
+            context.Logins.AddRange(
+                new Login
+                {
+                    LoginID = "12345678",
+                    CustomerID = 2100,
+                    PasswordHash = "YBNbEL4Lk8yMEWxiKkGBeoILHTU7WZ9n8jJSy8TNx0DAzNEFVsIVNRktiQV+I8d2"
+                },
+                new Login
+                {
+                    LoginID = "38074569",
+                    CustomerID = 2200,
+                    PasswordHash = "EehwB3qMkWImf/fQPlhcka6pBMZBLlPWyiDW6NLkAh4ZFu2KNDQKONxElNsg7V04"
+                },
+                new Login
+                {
+                    LoginID = "17963428",
+                    CustomerID = 2300,
+                    PasswordHash = "LuiVJWbY4A3y1SilhMU5P00K54cGEvClx5Y+xWHq7VpyIUe5fe7m+WeI0iwid7GE"
+                });
+
+            context.Accounts.AddRange(
+                new Account
+                {
+                    AccountNumber = 4100,
+                    //AccountType = AccountType.Saving,
+                    AccountType = 'S',
+                    CustomerID = 2100
+                },
+                new Account
+                {
+                    AccountNumber = 4101,
+                    //AccountType = AccountType.Checking,
+                    AccountType = 'C',
+                    CustomerID = 2100
+                },
+                new Account
+                {
+                    AccountNumber = 4200,
+                    //AccountType = AccountType.Saving,
+                    AccountType = 'S',
+                    CustomerID = 2200
+                },
+                new Account
+                {
+                    AccountNumber = 4300,
+                    //AccountType = AccountType.Checking,
+                    AccountType = 'C',
+                    CustomerID = 2300
+                });
+
+            const string openingBalance = "Opening balance";
+            const string format = "dd/MM/yyyy hh:mm:ss tt";
+            context.Transactions.AddRange(
+                new Transaction
+                {
+                    //TransactionType = TransactionType.Deposit,
+                    TransactionType = 'D',
+                    AccountNumber = 4100,
+                    Amount = 100,
+                    Comment = openingBalance,
+                    TransactionTimeUtc = DateTime.ParseExact("19/12/2019 08:00:00 PM", format, null)
+                },
+                new Transaction
+                {
+                    //TransactionType = TransactionType.Deposit,
+                    TransactionType = 'D',
+                    AccountNumber = 4101,
+                    Amount = 500,
+                    Comment = openingBalance,
+                    TransactionTimeUtc = DateTime.ParseExact("19/12/2019 08:30:00 PM", format, null)
+                },
+                new Transaction
+                {
+                    //TransactionType = TransactionType.Deposit,
+                    TransactionType = 'D',
+                    AccountNumber = 4200,
+                    Amount = 500.95m,
+                    Comment = openingBalance,
+                    TransactionTimeUtc = DateTime.ParseExact("19/12/2019 09:00:00 PM", format, null)
+                },
+                new Transaction
+                {
+                    //TransactionType = TransactionType.Deposit,
+                    TransactionType = 'D',
+                    AccountNumber = 4300,
+                    Amount = 1250.50m,
+                    Comment = "Opening balance",
+                    TransactionTimeUtc = DateTime.ParseExact("19/12/2019 10:00:00 PM", format, null)
+                });
+
+            context.SaveChanges();
+        }
+    }
+}
