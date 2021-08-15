be22365986edf2b2529aa31a9a5d6a7ab12f3714
s3318814 <61717348+gerardmanderson@users.noreply.github.com>
2021-08-15 21:24:26 +1000

    Changes to links and text

--- AdminWebsite/Models/LoginAdmin.cs
+++ AdminWebsite/Models/LoginAdmin.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace AdminWebsite.Models
 {
     public class LoginAdmin
     {
+        [Display(Name = "Username")]
         public string username { get; set; }
         public string password { get; set; }
     }
