2d067a07d9ab42dc786224134635f1912a9c6ab9
s3318814 <61717348+gerardmanderson@users.noreply.github.com>
2021-08-15 14:22:18 +1000

    Added transaction type name method and convert to local date.

--- IBCustomerSite/Models/Transaction.cs
+++ IBCustomerSite/Models/Transaction.cs
@@ -9,16 +9,20 @@ namespace IBCustomerSite.Models
     {
         [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         [Required]
+        [Display(Name = "Transaction ID")]
         public int TransactionId { get; set; }
 
         [Required]
+        [Display(Name = "Transaction Type")]
         public char TransactionType {get; set;}
 
         [Required]
+        [Display(Name = "Account Number")]
         public int AccountNumber { get; set; }
         public virtual Account Account { get; set; }
 
         [ForeignKey("DestinationAccount")]
+        [Display(Name = "Destination Account")]
         public int? DestinationAccountNumber { get; set; }
         public virtual Account DestinationAccount { get; set; }
 
@@ -31,6 +35,34 @@ namespace IBCustomerSite.Models
         public string Comment { get; set; }
 
         [Required]
+        [Display(Name = "Transaction Date")]
         public DateTime TransactionTimeUtc { get; set; }
+
+
+
+        public string TransactionTypeName()
+        {
+            switch (TransactionType)
+            {
+                case 'D':
+                    return "Deposit";
+                case 'W':
+                    return "Withdrawal";
+                case 'T':
+                    return "Transfer";
+                case 'B':
+                    return "BillPay";
+                default:
+                    return "Service Charge";
+
+            }
+        }
+
+        public DateTime ReturnLocalTime()
+        {
+            return TransactionTimeUtc.ToLocalTime();
+        }
     }
+
+
 }
